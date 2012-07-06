c303e2048db28fdd07ba6ba33c49a648bb83b351
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2012-07-06 18:01:09 -0400

    2012-07-06 - Jonathan Morgan - added in twitter users, and implemented some samples of interacting with twitter through OAuth.

--- models.py
+++ models.py
@@ -9,13 +9,13 @@
 
 from django.db import models
 
-class Tweets( models.Model ):
+class Tweet( models.Model ):
 
     tweet_timestamp = models.CharField( max_length = 255, blank = True )
     tweet_timestamp_datetime = models.DateTimeField( null = True, blank = True )
     twitter_tweet_id = models.BigIntegerField()
-    twitter_user_id = models.IntegerField( null = True, blank = True )
-    twitter_user_screenname = models.CharField( max_length = 255, primary_key = True )
+    twitter_user = models.ForeignKey( 'Twitter_User' )
+    twitter_user_screenname = models.CharField( max_length = 255 )
     tweet_retweet_count = models.IntegerField( null = True, blank = True )
     tweet_location = models.TextField( null = True, blank = True )
     tweet_has_location = models.IntegerField( null = True, blank = True )
@@ -64,4 +64,127 @@ class Tweets( models.Model ):
     timestamp_minute = models.IntegerField( null = True, blank = True )
     timestamp_second = models.IntegerField( null = True, blank = True )
 
-#-- END class NewsTweets --#
+    #----------------------------------------------------------------------
+    # instance methods
+    #----------------------------------------------------------------------
+
+    def __unicode__( self ):
+ 
+        # return reference
+        string_OUT = ''
+ 
+        if ( self.id ):
+        
+            string_OUT = str( self.id ) + " - "
+            
+        #-- END check to see if ID --#
+                
+        string_OUT += self.twitter_tweet_id
+        
+        # screenname?
+        if ( self.twitter_user_screenname ):
+        
+            string_OUT += " ( " + self.twitter_user_screenname
+            
+            # link to Twitter_User?
+            if ( self.twitter_user ):
+            
+                # yes.  Output ID.
+                string_OUT += " - " + self.twitter_user.id
+            
+            #-- END check to see if twitter user. --#
+            
+            string_OUT += " )"
+            
+        #-- END screenname check --#
+
+        if ( self.tweet_text ):
+        
+            string_OUT += + ": " + self.tweet_text
+            
+        #-- END check to see if we have tweet text. --#
+ 
+        return string_OUT
+
+    #-- END method __unicode__() --#
+
+#-- END class Tweet --#
+
+
+class Twitter_User( models.Model ):
+
+    twitter_user_name = models.CharField( max_length = 255, blank=True )
+    tweet_count = models.IntegerField( null = True, blank = True )
+    tweet_retweet_count_mean = models.FloatField( null = True, blank = True )
+    tweet_retweet_count_sum = models.IntegerField( null = True, blank = True )
+    user_follower_count_max = models.IntegerField( null = True, blank = True )
+    user_favorites_count_max = models.IntegerField( null = True, blank = True )
+    tweet_user_mention_count_mean = models.FloatField( null = True, blank = True )
+    tweet_user_mention_count_sum = models.IntegerField( null = True, blank = True )
+    tweet_hashtag_mention_count_mean = models.FloatField( null = True, blank = True )
+    tweet_hashtag_mention_count_sum = models.IntegerField( null = True, blank = True )
+    tweet_url_count_mean = models.FloatField( null = True, blank = True )
+    tweet_url_count_sum = models.IntegerField( null = True, blank = True )
+    tweet_text_length_mean_1 = models.FloatField( null = True, blank = True )
+    tweet_text_length_median = models.FloatField( null = True, blank = True )
+    is_bbc_sum = models.IntegerField( null = True, blank = True )
+    is_cbs_sum = models.IntegerField( null = True, blank = True )
+    is_cnn_sum = models.IntegerField( null = True, blank = True )
+    is_drudge_sum = models.IntegerField( null = True, blank = True )
+    is_fox_sum = models.IntegerField( null = True, blank = True )
+    is_huffington_sum = models.IntegerField( null = True, blank = True )
+    is_msnbc_sum = models.IntegerField( null = True, blank = True )
+    is_nyt_sum = models.IntegerField( null = True, blank = True )
+    is_npr_sum = models.IntegerField( null = True, blank = True )
+    is_usatoday_sum = models.IntegerField( null = True, blank = True )
+    is_wsj_sum = models.IntegerField( null = True, blank = True )
+    is_wpo_sum = models.IntegerField( null = True, blank = True )
+    is_liberal_sum = models.IntegerField( null = True, blank = True )
+    is_control_sum = models.IntegerField( null = True, blank = True )
+    is_conservative_sum = models.IntegerField( null = True, blank = True )
+    outlet_heterogeneity_low_sum = models.IntegerField( null = True, blank = True )
+    outlet_heterogeneity_med_sum = models.IntegerField( null = True, blank = True )
+    outlet_heterogeneity_high_sum = models.IntegerField( null = True, blank = True )
+    is_bbc_max = models.IntegerField( null = True, blank = True )
+    is_cbs_max = models.IntegerField( null = True, blank = True )
+    is_cnn_max = models.IntegerField( null = True, blank = True )
+    is_drudge_max = models.IntegerField( null = True, blank = True )
+    is_fox_max = models.IntegerField( null = True, blank = True )
+    is_huffington_max = models.IntegerField( null = True, blank = True )
+    is_msnbc_max = models.IntegerField( null = True, blank = True )
+    is_nyt_max = models.IntegerField( null = True, blank = True )
+    is_npr_max = models.IntegerField( null = True, blank = True )
+    is_usatoday_max = models.IntegerField( null = True, blank = True )
+    is_wsj_max = models.IntegerField( null = True, blank = True )
+    is_wpo_max = models.IntegerField( null = True, blank = True )
+    diversity = models.FloatField( null = True, blank = True )
+    ideology_count = models.IntegerField( null = True, blank = True )
+    ideology_code = models.IntegerField( null = True, blank = True )
+
+    #----------------------------------------------------------------------
+    # instance methods
+    #----------------------------------------------------------------------
+
+    def __unicode__( self ):
+ 
+        # return reference
+        string_OUT = ''
+ 
+        if ( self.id ):
+        
+            string_OUT = str( self.id ) + " - "
+            
+        #-- END check to see if ID --#
+                
+        # screenname?
+        if ( self.twitter_user_name ):
+        
+            string_OUT += self.twitter_user_name
+            
+        #-- END screenname check --#
+
+        return string_OUT
+
+    #-- END method __unicode__() --#
+
+#-- END class Twitter_User --#
