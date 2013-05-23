c22f98d6a977510c4e1adfdf154793dace43f603
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2013-05-23 09:50:41 -0400

    2013-05-23 - Jonathan Morgan - abstract classes.

--- models.py
+++ models.py
@@ -1,8 +1,24 @@
+'''
+Copyright 2012, 2013 Jonathan Morgan
+
+This file is part of http://github.com/jonathanmorgan/tweetnet.
+
+tweetnet is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+tweetnet is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with http://github.com/jonathanmorgan/tweetnet. If not, see http://www.gnu.org/licenses/.
+'''
+from __future__ import unicode_literals
+
 from django.db import models
+import django.utils.encoding
+from django.utils.encoding import python_2_unicode_compatible
 
 # not abstract for now.
-# class Abstract_Tweet( models.Model ):
-class Tweet( models.Model ):
+@python_2_unicode_compatible
+class Abstract_Tweet( models.Model ):
+
 
     # timestamp
     tweet_timestamp = models.CharField( max_length = 255, blank = True )
@@ -60,17 +76,16 @@ class Tweet( models.Model ):
     # meta
     #----------------------------------------------------------------------
 
-    # not abstract for now.
     # meta class so we know this is an abstract class.
-    #class Meta:
-    #    abstract = True
+    class Meta:
+        abstract = True
 
 
     #----------------------------------------------------------------------
     # instance methods
     #----------------------------------------------------------------------
 
-    def __unicode__( self ):
+    def __str__( self ):
  
         # return reference
         string_OUT = ''
@@ -108,15 +123,63 @@ class Tweet( models.Model ):
  
         return string_OUT
 
-    #-- END method __unicode__() --#
+    #-- END method __str__() --#
 
-#-- END class Tweet --#
 #-- END class Abstract_Tweet --#
 
 
+@python_2_unicode_compatible
+class Tweet( Abstract_Tweet ):
+
+
+    def __str__( self ):
+ 
+        # return reference
+        string_OUT = ''
+ 
+        if ( self.id ):
+        
+            string_OUT = str( self.id ) + " - "
+            
+        #-- END check to see if ID --#
+                
+        string_OUT += self.twitter_tweet_id
+        
+        # screenname?
+        if ( self.twitter_user_screenname ):
+        
+            string_OUT += " ( " + self.twitter_user_screenname
+            
+            # link to Twitter_User?
+            if ( self.twitter_user ):
+            
+                # yes.  Output ID.
+                string_OUT += " - " + self.twitter_user.id
+            
+            #-- END check to see if twitter user. --#
+            
+            string_OUT += " )"
+            
+        #-- END screenname check --#
+
+        if ( self.tweet_text ):
+        
+            string_OUT += + ": " + self.tweet_text
+            
+        #-- END check to see if we have tweet text. --#
+ 
+        return string_OUT
+
+    #-- END method __str__() --#
+
+
+#-- END class Tweet --#
+
+
 # not abstract for now.
-# class Abstract_Twitter_User( models.Model ):
-class Twitter_User( models.Model ):
+@python_2_unicode_compatible
+class Abstract_Twitter_User( models.Model ):
+
 
     twitter_user_name = models.CharField( max_length = 255, blank=True )
     twitter_user_twitter_id = models.BigIntegerField()
@@ -126,11 +189,42 @@ class Twitter_User( models.Model ):
     # meta
     #----------------------------------------------------------------------
 
-    # not abstract for now.
     # meta class so we know this is an abstract class.
-    #class Meta:
-    #    abstract = True
+    class Meta:
+        abstract = True
 
+
+    #----------------------------------------------------------------------
+    # instance methods
+    #----------------------------------------------------------------------
+
+    def __unicode__( self ):
+ 
+        # return reference
+        string_OUT = ''
+ 
+        if ( self.id ):
+        
+            string_OUT = str( self.id ) + " - "
+            
+        #-- END check to see if ID --#
+                
+        # screenname?
+        if ( self.twitter_user_name ):
+        
+            string_OUT += self.twitter_user_name
+            
+        #-- END screenname check --#
+
+        return string_OUT
+
+    #-- END method __unicode__() --#
+
+#-- END class Abstract_Twitter_User --#
+
+
+@python_2_unicode_compatible
+class Twitter_User( Abstract_Twitter_User ):
 
     #----------------------------------------------------------------------
     # instance methods
